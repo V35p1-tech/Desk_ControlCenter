025513dac3dac115f272474efb5bbb78072c01e7
WD <d.widawski@versor.pl>
2024-03-21 16:57:06 +0100

    display

--- Desk_ControlPanel/src/main.cpp
+++ Desk_ControlPanel/src/main.cpp
@@ -3,19 +3,21 @@
 #include <SPI.h>
 #include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int Display_reset = 0;
+int Display_CLK = 1;
+int Display_DT = 2; 
+
+
+U8G2_SH1106_128X64_NONAME_F_HW_I2C Display(U8G2_R0, U8X8_PIN_NONE, Display_CLK, Display_DT) [full framebuffer, size = 1024 bytes];
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ Display.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Display.firstPage();
+  do {
+    Display.setFont(u8g2_font_ncenB14_tr);
+    Display.drawStr(0,24,"Hello World!");
+  } while ( Display.nextPage() );
 }
