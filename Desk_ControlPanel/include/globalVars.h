b55b9e5bb3b447c6f3e757ca0cd55c855304dcf9
WD <d.widawski@versor.pl>
2024-04-04 15:44:01 +0200

    kjh

--- Desk_ControlPanel/include/globalVars.h
+++ Desk_ControlPanel/include/globalVars.h
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 //Screen
-    extern int Display_reset = 0;
-    extern int Display_CLK = 1;
-    extern int Display_DT = 2; 
+    extern int Display_reset = 2;
+    extern int Display_CLK = 0;
+    extern int Display_DT = 1; 
 //Encoder 
     extern int EncoderChA; //pin 3
     extern int EncoderChB; //pin 4
@@ -15,4 +15,45 @@
     extern bool Relay_5; //pin 10 
     extern bool Relay_6; //pin 11 
     extern bool Relay_7; //pin 12 
-    extern bool Relay_8; //pin 13    
+    extern bool Relay_8; //pin 13   
+//Screens
+    extern int Screen_Selected;
+    extern const int Screen_1 = 1;  
+    extern const int Screen_2 = 2;   
+    extern const int Screen_3 = 3; 
+//Rows Y
+    extern int Row_Selected; 
+    extern int Row_Height = 10; 
+
+    extern const int Row_1 = 1;  
+    extern const int Row_2 = 2;  
+    extern const int Row_3 = 3;  
+    extern const int Row_4 = 4;  
+    extern const int Row_5 = 5;   
+    extern int Row_1_X = 12;
+    extern int Row_2_X = 24;
+    extern int Row_3_X = 36;
+    extern int Row_4_X = 48;
+    extern int Row_5_X = 60;
+    extern int Row_Text_Y = 20;
+    extern char Row_1_Text[30] = "Wiersz 1";
+    extern char Row_2_Text[30] = "Wiersz 2";
+    extern char Row_3_Text[30] = "Wiersz 3";
+    extern char Row_4_Text[30] = "Wiersz 4";
+    extern char Row_5_Text[30] = "Nastepna strona";
+
+    struct RowData {
+        char Text[100];
+        bool Activated;
+    };
+    struct screenData 
+    {
+        RowData Row1;
+        RowData Row2;
+        RowData Row3;
+        RowData Row4;
+        RowData Row5;
+    };
+    extern screenData Screen[5];
+    extern screenData ScreenActual;
+  
--- Desk_ControlPanel/include/screenHandling.cpp
+++ Desk_ControlPanel/include/screenHandling.cpp
@@ -6,12 +6,132 @@ U8G2_SH1106_128X64_NONAME_F_HW_I2C Display(U8G2_R0, U8X8_PIN_NONE, Display_CLK, 
 
 void setup() {
  Display.begin();
+   Display.firstPage();
+  do {
+    Display.setFont(u8g2_font_ncenB14_tr);
+    Display.drawStr(0,24,"Centrum dowodzenia");
+  } while ( Display.nextPage() );
+
+    delay(1000);
+    Display.clearDisplay();
+    Row_Selected = 1;
+    Screen_Selected = Screen_1;
 }
 
 void loop() {
-  Display.firstPage();
-  do {
-    Display.setFont(u8g2_font_ncenB14_tr);
-    Display.drawStr(0,24,"Hello World!");
-  } while ( Display.nextPage() );
+
+    ScreenActual = Screen[Screen_Selected];
+    delay(100);
+    
+    switch (Screen_Selected) 
+
+        case Screen_1:
+            //Screen_1
+            strcpy(Screen[Screen_1].Row1.Text, "Strona 1 -Wiersz 1");
+            strcpy(Row_1_Text,"Strona 1 -Wiersz 1");
+            strcpy(Row_2_Text,"Strona 1 -Wiersz 2");
+            strcpy(Row_3_Text,"Strona 1 -Wiersz 3");
+            strcpy(Row_4_Text,"Strona 1 -Wiersz 2");
+            // DECLARE HERE LABELS FOR ROWS ^^
+            switch (Row_Selected){
+                case Row_1:
+                    //Row1 
+                        Display.drawDisc(5,(Row_1_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_1_X,Row_1_Text);
+                    //Row2
+                        Display.drawCircle(5,(Row_2_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_2_X,Row_2_Text);
+                    //Row3
+                        Display.drawCircle(5,(Row_3_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_3_X,Row_3_Text);
+                    //Row4
+                        Display.drawCircle(5,(Row_4_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_4_X,Row_4_Text);
+                    //Row5
+                        Display.drawCircle(5,(Row_5_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_5_X,Row_5_Text);     
+                break;
+                case Row_2:
+                    //Row1 
+                        Display.drawCircle(5,(Row_1_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_1_X,Row_1_Text);
+                    //Row2
+                        Display.drawDisc(5,(Row_2_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_2_X,Row_2_Text);
+                    //Row3
+                        Display.drawCircle(5,(Row_3_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_3_X,Row_3_Text);
+                    //Row4
+                        Display.drawCircle(5,(Row_4_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_4_X,Row_4_Text);
+                    //Row5
+                        Display.drawCircle(5,(Row_5_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_5_X,Row_5_Text);  
+                break;
+                case Row_3:
+                    //Row1 
+                        Display.drawCircle(5,(Row_1_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_1_X,Row_1_Text);
+                    //Row2
+                        Display.drawCircle(5,(Row_2_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_2_X,Row_2_Text);
+                    //Row3
+                        Display.drawDisc(5,(Row_3_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_3_X,Row_3_Text);
+                    //Row4
+                        Display.drawCircle(5,(Row_4_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_4_X,Row_4_Text);
+                    //Row5
+                        Display.drawCircle(5,(Row_5_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_5_X,Row_5_Text); 
+                break;
+                case Row_4:
+                    //Row1 
+                        Display.drawCircle(5,(Row_1_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_1_X,Row_1_Text);
+                    //Row2
+                        Display.drawCircle(5,(Row_2_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_2_X,Row_2_Text);
+                    //Row3
+                        Display.drawCircle(5,(Row_3_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_3_X,Row_3_Text);
+                    //Row4
+                        Display.drawDisc(5,(Row_4_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_4_X,Row_4_Text);
+                    //Row5
+                        Display.drawCircle(5,(Row_5_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_5_X,Row_5_Text); 
+                break;               
+                case Row_5:
+                    //Row1 
+                        Display.drawCircle(5,(Row_1_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_1_X,Row_1_Text);
+                    //Row2
+                        Display.drawCircle(5,(Row_2_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_2_X,Row_2_Text);
+                    //Row3
+                        Display.drawCircle(5,(Row_3_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_3_X,Row_3_Text);
+                    //Row4
+                        Display.drawCircle(5,(Row_4_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_4_X,Row_4_Text);
+                    //Row5
+                        Display.drawDisc(5,(Row_5_X-(Row_Height/2)),(Row_Height/2),U8G2_DRAW_ALL);
+                        Display.drawStr(Row_Text_Y,Row_5_X,Row_5_Text); 
+                break;
+            }
+        break;
+        case Screen_2:
+            //Screen 2
+            strcpy(Row_1_Text,"Strona 2 -Wiersz 1");
+            strcpy(Row_2_Text,"Strona 2 -Wiersz 2");
+            strcpy(Row_3_Text,"Strona 2 -Wiersz 3");
+            strcpy(Row_4_Text,"Strona 2 -Wiersz 2");
+            switch (Row_Selected){
+
+
+                }
+            break;
+    }
+
 }
